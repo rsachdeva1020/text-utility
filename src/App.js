8625a637d8528027120a07303e12111687adc7ca
rsachdeva1020 <rohansachdeva1020@gmail.com>
2021-09-06 23:03:48 +0530

    Routing and favicon added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, {useState} from 'react'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
 import About from './components/About'
 import Alert from './components/Alert';
+
+
 
 function App() {
   const [mode, setMode] = useState('light');//will tell whether dark mode is enabled or not
@@ -23,22 +33,32 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = 'rgb(0 0 0 / 75%)'
       showAlert("Dark mode has been enabled", "success",)
+      // document.title = 'TextUtils | Dark'  // changing title dynamically
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white'
       showAlert("Light mode has been enabled", "success")
+      // document.title = 'TextUtils | Home'
     }
   }
 
   return (
     <>
+    <Router>
       <Navbar title = "TextUtils" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter the text below to analyse" mode={mode} showAlert={showAlert}/>
+        <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm heading="Enter the text below to analyse" mode={mode} showAlert={showAlert}/>
+            </Route>
+          </Switch>
       </div>
-      { /* <About/> */}
+    </Router>
     </>
   );
 }
