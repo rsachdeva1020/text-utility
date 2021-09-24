10da94d501aeed965b9780ca503632ad6468b540
rsachdeva1020 <rohansachdeva1020@gmail.com>
2021-09-25 01:59:22 +0530

    minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,14 +22,8 @@ export default function Navbar(props) {
                     </ul>
                     <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
                         <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onClick={props.toggleMode}/>
-                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable {props.mode === 'dark' ? 'Light' : 'Dark'} Mode</label>
                     </div>
-                    {
-                    // <form className="d-flex">
-                    //     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    //     <button className="btn btn-outline-success" type="submit">Search</button>
-                    // </form>
-                    }
                 </div>
             </div>
         </nav>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,13 +15,6 @@ export default function TextForm(props) {
         setText('')
         props.showAlert("Text Cleared", "success")
     }
-    // function handleCapitalClick(){
-    //     let temp = text;
-    //     let newText = temp.split(" ");
-    //     newText.forEach(text => text.charAt(0).toUpperCase() + text.slice(1))
-    //     newText.join(" ")
-    //     setText(newText);
-    // }
     
     function copyToClipBoard(){
         navigator.clipboard.writeText(text);
@@ -39,20 +32,35 @@ export default function TextForm(props) {
         setText(newText.join(" "));
         props.showAlert("Text Capitalised", "success")
     }
+    function handleFrequency(){
+        for(let i = 0;i<text.length;i++)
+            if((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z')) 
+                counts[text[i]] = counts[text[i]] ? counts[text[i]] + 1 : 1
+        keys = Object.keys(counts)
+
+        let a = keys.map((key, index) => key + " - " + counts[key])
+
+        let newText = a.join(", ");
+
+        setFrequency(newText)
+        // console.log(newText)
+    }
 
     function handleOnChange(e){
         setText(e.target.value)
     }
     
+    let keys, counts = {};
     const [text, setText] = useState('');
+    const [frequency, setFrequency] = useState("")
 
     return (
     <>
     <div>
         <div className="container" style={{color : props.mode === 'dark' ? 'white' : 'black'}}>
             <div className="mb-3">
-                <h1 className="mb-20">{props.heading}</h1>
-                <textarea className="form-control" style={{backgroundColor : props.mode === 'dark' ? 'rgb(42 42 42 / 80%)' : 'white', color : props.mode === 'dark' ? 'white' : 'black'}} onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
+                    <h1 className="mb-20">{props.heading}</h1>
+                    <textarea className="form-control" style={{backgroundColor : props.mode === 'dark' ? 'rgb(42 42 42 / 80%)' : 'white', color : props.mode === 'dark' ? 'white' : 'black'}} onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
             </div>
             <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to Upper Case</button>
             <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to Lower Case</button>
@@ -60,16 +68,14 @@ export default function TextForm(props) {
             <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1" onClick={copyToClipBoard}>Copy to clipboard</button>
             <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1" onClick={handleCapitalise}>Capitalise Text</button>
-            </div>
+            <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1" onClick={handleFrequency}>Frequency</button>
+        </div>
         <div className="container my-4" style={{color : props.mode === 'dark' ? 'white' : 'black'}}>
             <h2> Your Text Summary</h2>
             <p>{text.split(/\s+/).filter(ele => ele.length !== 0).length} words and {text.length} characters</p>
             <p>{(0.008 * text.split(" ").filter(ele => ele.length !== 0).length).toFixed(3)} Minutes read</p>
-            {/*
-                for(let i = 0;i<text.length;i++) if(text[i] !== " ") counts[text[i]] = counts[text[i]] ? counts[text[i]] + 1 : 1
-                keys = Object.keys(counts)
-            */}
-            {/* text.length > 0 ? keys.forEach((key, index) => console.log(`${key}: ${counts[key]}`)) : " " */}
+            {/*frequency*/}
+
             {//<h3>Preview</h3>
             // <pre>{text.length > 0 ? text : "Enter some text above to preview here"} </pre>
             }
